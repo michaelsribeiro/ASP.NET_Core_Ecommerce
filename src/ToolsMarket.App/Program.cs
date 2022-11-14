2cc18483dcb334d1236bafd53a09bda7850a54dc
Michael Ribeiro <m.ribeiroabd@gmail.com>
2022-11-14 02:08:25 -0300

    Deploy Azure

--- src/ToolsMarket.App/Program.cs
+++ src/ToolsMarket.App/Program.cs
@@ -38,16 +38,8 @@ builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseDeveloperExceptionPage();
+app.UseMigrationsEndPoint();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
